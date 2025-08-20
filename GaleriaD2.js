638df60003710543e630637753198acf283bb27e
Raked <75103024+iRaked@users.noreply.github.com>
2025-08-20 13:44:45 -0600

    Add files via upload

---
+++ GaleriaD2.js
@@ -0,0 +1,11 @@
+const count = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount(){
+  fetch('https://api.countapi.xyz/update/popsi/popsi/?amount=1')
+    .then(res => res.json())
+    .then(res => {
+      count.innerHTML = res.value
+  });
+}
