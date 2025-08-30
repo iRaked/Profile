2c224894cfc31e46c362258009111b92fb396bf0
Raked <75103024+iRaked@users.noreply.github.com>
2025-08-29 18:16:26 -0600

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,19 @@
-// firebaseConfig.js
-export const firebaseConfig = {
-  apiKey: "AIzaSyAYDEA7pJQOkhdqCbUyfW9EwvxfEesOweE",
-  authDomain: "telegrafo-24ad3.firebaseapp.com",
-  projectId: "telegrafo-24ad3",
-  storageBucket: "telegrafo-24ad3.appspot.com",
-  messagingSenderId: "594000910376",
-  appId: "1:594000910376:web:49790452ecfd1ed3077d1d"
-};
+import { db } from './firebaseConfig';
+import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+
+document.getElementById("sendMessage").addEventListener("click", async () => {
+  const message = document.getElementById("fanMessage").value;
+
+  if (message.trim() === "") return;
+
+  try {
+    await addDoc(collection(db, "messages"), {
+      texto: message,
+      timestamp: serverTimestamp()
+    });
+    document.getElementById("fanMessage").value = ""; // Limpieza ritual
+    console.log("Mensaje enviado a Rick");
+  } catch (error) {
+    console.error("Error al enviar el mensaje:", error);
+  }
+});
